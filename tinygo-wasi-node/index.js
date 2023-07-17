86a93840736fc801092cc3a917388e9843add3a1
Alan Wang <krantas@gmail.com>
2023-07-17 19:24:47 +0800

    Update index.js

--- tinygo-wasi-node/index.js
+++ tinygo-wasi-node/index.js
@@ -3,17 +3,19 @@ import fs from "fs";
 
 (async () => {
   await init();
-
   const wasi = new WASI({
     env: {},
     args: [],
   });
+  
   const buf = fs.readFileSync("./go/main.wasm");
   const module = await WebAssembly.compile(new Uint8Array(buf));
 
-  // or
-  // const response = fetch("http://github.com/alankrantas/wasm_experiments/raw/main/tinygo-wasi-node/src/main.wasm");
-  // const module = await WebAssembly.compileStreaming(response);
+  // or:
+  // const res = fetch("http://github.com/alankrantas/wasm_experiments/raw/main/tinygo-wasi-node/src/main.wasm");
+  // const module = await WebAssembly.compileStreaming(res);
+  // or:
+  // const module = await WebAssembly.compile(await res.arrayBuffer());
   
   const instance = wasi.instantiate(module, {});
 
@@ -22,6 +24,5 @@ import fs from "fs";
   console.log(`main: ${stdout}(exit code: ${exitCode})`);
   
   const add = instance.exports.add;
-  console.log(`add: ${add(2, 3)}`);  // invoke add()
-  
+  console.log(`add: ${add(40, 2)}`);  // invoke add()
 })();
