502474b19b53608ed7ccf42baefb080360f2f99b
Alan Wang <krantas@gmail.com>
2023-07-18 10:38:41 +0800

    Update index.js

--- tinygo-wasi-node/index.js
+++ tinygo-wasi-node/index.js
@@ -1,28 +1,32 @@
+const fileName = "./src/main.wasm";
+const url =
+  "http://github.com/alankrantas/wasm_experiments/raw/main/tinygo-wasi-node/src/main.wasm";
+
 import { init, WASI } from "@wasmer/wasi";
 import fs from "fs";
 
 (async () => {
   await init();
+
   const wasi = new WASI({
     env: {},
     args: [],
   });
-  
-  const buf = fs.readFileSync("./src/main.wasm");
-  const module = await WebAssembly.compile(new Uint8Array(buf));
 
-  // or:
-  // const res = fetch("http://github.com/alankrantas/wasm_experiments/raw/main/tinygo-wasi-node/src/main.wasm");
-  // const module = await WebAssembly.compileStreaming(res);
-  // or:
-  // const module = await WebAssembly.compile(await res.arrayBuffer());
-  
+  /*
+  const module = await WebAssembly.compile(
+    await (await fetch(url)).arrayBuffer()
+  );
+  */
+  const module = await WebAssembly.compile(fs.readFileSync(fileName));
   const instance = wasi.instantiate(module, {});
 
-  const exitCode = wasi.start(instance);  // invoke main()
-  const stdout = wasi.getStdoutString();  // read the stdout from main()
-  console.log(`main: ${stdout}(exit code: ${exitCode})`);
-  
+  // console.log(instance.exports);  // list of exports
   const add = instance.exports.add;
-  console.log(`add: ${add(40, 2)}`);  // invoke add()
+
+  const exitCode = wasi.start(instance);  // invoke main()
+  const stdout = wasi.getStdoutString();  // stdout of main()
+
+  console.log(`add: ${add(2, 3)}`);  // invoke add()
+  console.log(`main: ${stdout}(exit code: ${exitCode})`);
 })();
